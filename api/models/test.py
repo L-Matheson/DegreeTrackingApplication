278addd3b92747a0a0aa37e061e629cd733e08c8
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2025-02-10 18:27:42 -0500

    Added frontend react app, More docker work

--- api/models/test.py
+++ api/models/test.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default="John Doe")
     age = models.PositiveIntegerField()
 
     def __str__(self):
--- api/urls.py
+++ api/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from api.view_index import home
 from api.views.testview import PersonListView
 
-
-
-from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
